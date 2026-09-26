43461dc70928613809ee8085802109a4a7fec9f1
anjana55 <anjana55@hotmail.com>
2026-09-26 09:22:25 +0530

    next.js config updated

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/staff',
   reactStrictMode: true,
+  // assetPrefix: '/staff',   // usually not needed when using basePath
 };
 
 module.exports = nextConfig;
